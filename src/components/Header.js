c53511995ef9a78ee2c07ea2ac665d2714d45607
victrosantos <victor.santos.fk@hotmail.com>
2022-10-31 16:27:29 -0300

    [Apply] Feito req 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,36 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  componentDidMount() {
+    const { history, email } = this.props;
+    if (!email) history.push('/');
+  }
+
   render() {
+    const { email, idToEdit } = this.props;
     return (
-      <div>Header</div>
+      <header>
+        <p data-testid="email-field">{email}</p>
+        <p data-testid="total-field">{idToEdit}</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </header>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+  idToEdit: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+  idToEdit: state.wallet.idToEdit,
+});
+
+export default connect(mapStateToProps)(Header);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,21 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { history } = this.props;
+    return (
+      <>
+        <Header history={ history } />
+        <div>TrybeWallet</div>
+      </>
+    );
   }
 }
+
+Wallet.propTypes = {
+  history: PropTypes.shape({}).isRequired,
+};
 
 export default Wallet;
